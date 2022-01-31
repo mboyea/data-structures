e8dae2bcb12333e5c59c4ef031fef50d10d9f643
YourZombieMop <68197922+YourZombieMop@users.noreply.github.com>
2022-01-31 10:38:09 -0600

    BinarySearchRecursive
    Completed the short BinarySearchRecursive project

--- 4-BinarySearchRecursive/MatthewBoyeaBinarySearchRecursive.cpp
+++ 4-BinarySearchRecursive/MatthewBoyeaBinarySearchRecursive.cpp
@@ -1,16 +1,46 @@
 #include <iostream>
 
-int FibonacciRecursive(int termIndex) {
-	if (termIndex == 0) {
-		return 0;
-	} else if (termIndex == 1) {
-		return 1;
-	} else return FibonacciRecursive(termIndex - 1) + FibonacciRecursive(termIndex - 2);
+
+// Precondition: array is sorted from least to greatest
+// Time Complexity: O(log_2(n))
+int BinarySearchRecursive(const int* array, const int size, const int key, int low, int high) {
+	if (low > high) {
+		// key not found
+		return -1;
+	}
+
+	int mid = (low + high) / 2;
+	if (array[mid] < key) {
+		// element is to the right
+		return BinarySearchRecursive(array, size, key, mid + 1, high);
+	}	else if (array[mid] > key) {
+		// element is to the left
+		return BinarySearchRecursive(array, size, key, low, mid - 1);
+	}
+	// key found
+	return mid;
+}
+
+// Precondition: array is sorted from least to greatest
+// Time Complexity: O(log_2(n))
+int BinarySearchRecursive(const int* array, const int size, const int key) {
+	return BinarySearchRecursive(array, size, key, 0, size-1);
 }
 
 int main() {
 	// Show title
-	std::cout << "FIBONACCI NUMBER CALCULATOR\n---\n";
+	std::cout << "RECURSIVE BINARY SEARCH\n---\n";
+
+	// Prepare numbers
+	const int NUMBERS_SIZE = 8;
+	int numbers[NUMBERS_SIZE] = { 2, 4, 7, 10, 11, 32, 45, 87 };
+
+	// Show numbers
+	std::cout << "NUMBERS: ";
+	for (int i = 0; i < NUMBERS_SIZE; i++) {
+		std::cout << numbers[i] << ' ';
+	}
+	std::cout << '\n';
 
 	// Get target from user
 	std::cout << "Enter a target: ";
@@ -18,10 +48,13 @@ int main() {
 	std::cin >> target;
 
 	// Find target index in numbers array
-	int result = FibonacciRecursive(target);
+	int targetIndex = BinarySearchRecursive(numbers, NUMBERS_SIZE, target);
 
-	// Return result to user
-	std::cout << "Fibonacci Number: " << result << "\n\n";
+	// Return target index to user
+	if (targetIndex < 0) {
+		std::cout << "Target was not found in the array.";
+	} else std::cout << "Target was found at index: " << targetIndex;
+	std::cout << "\n\n";
 
 	// Pause
 	system("pause");
